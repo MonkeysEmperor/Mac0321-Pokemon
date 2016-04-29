5d8b6cf8faef25052b8e1a0bf084f8ffb60d8a12
rafalencar1997 <rafael.alencar@usp.br>
2016-04-29 16:44:58 -0300

    Added files via upload
    Ainda falta arrumar o jeito que essa classe chama o controlador de batalhas mas a lógica ta quase pronta

--- JogoMapa.java
+++ JogoMapa.java
@@ -1,54 +1,27 @@
 package Ex1;
-import java.util.Arrays;
 import java.util.Random;
+import Ex1.BattleControls;
 
-
-public class JogoMapa {
+public class JogoMapa extends Controller{
 	
 	boolean jogo= false;
 
-	char local;
-	int i = 0, j = 0;
+	char local = ' ';
+	int i = 0, j = 0, x= 0;
 
 	static int n = 7;
 	Trainer wild = new Trainer("WILD");
 	
-	
-	
-	char[] comando = { 'd', 'd','s','s','d','d','s','s','a','w','a','s','s','d','d','d','s','a','w','w','d','s','a','a','w','s','d'};
-	char[][] map = {  { ' ', ' ', ' ', ' ', ' ' , ' ', ' '}, 
+	char[] comando = { 
+			'd', 'd', 's','s','d','d','s','s','a','w','a','s','s','d','d','d','s','a','w','w','d','s','a','a','w','s','d'};
+	char[][] map = {
+			{ 'P', ' ', ' ', ' ', ' ' , ' ', ' '}, 
 					  { ' ', 'g', 'g', ' ', 'g' , 'g', ' '},
 					  { ' ', 'g', 'g', ' ', 'g' , 'g', ' '},			
 					  { ' ', ' ', ' ', ' ', ' ' , ' ', ' '},
 					  { ' ', 'g', 'g', 'g', 'g' , 'g', ' '},
 					  { ' ', 'g', 'g', 'g', 'g' , 'g', ' '},
 					  { ' ', ' ', ' ', ' ', ' ' , ' ', ' '}};
-	
-	void anda( char seta){
-		if(seta == 'w') 
-			if(i > 0){
-				map[i][j] = local;
-				i-= 1;
-			}
-		if(seta == 'a')
-			if(j > 0) {
-				map[i][j] = local;
-				j-= 1;
-			}
-		if(seta == 's')
-			if(i < n-1){
-				map[i][j] = local;
-				i+= 1;
-			}
-		if(seta == 'd')
-			if(j < n-1){
-				map[i][j] = local;
-				j+= 1;
-			}
-		local = map[i][j];
-		map[i][j] = 'P';		
-	}
-	
 	
 	void gramado(double numero){
 	    if(numero < 60){
@@ -72,48 +45,82 @@ public class JogoMapa {
 	    wild.pokemon = 1;
 	}
 	
-	void tela(){
+	private class Jogo extends Event{
 		
-		for(int b = 0; b < n; b++)
-			System.out.print(" _");
-		System.out.print("\n");
-		for(int a = 0; a < n; a++){
-			for(int b = 0; b < n; b++){
-				
-				if(b == 0){
-					System.out.print('|');
-					if(a == n-1 && map[a][b] == ' ') System.out.print('_');
-					else System.out.print(map[a][b]);
+		private void anda( char seta){
+			if(seta == 'w') 
+				if(i > 0){
+					map[i][j] = local;
+					i-= 1;
 				}
-				else{
-					if(a == n-1 && map[a][b] == ' ') System.out.print(" _");
-					else System.out.print(" " + map[a][b]);
+			if(seta == 'a')
+				if(j > 0) {
+					map[i][j] = local;
+					j-= 1;
 				}
+			if(seta == 's')
+				if(i < n-1){
+					map[i][j] = local;
+					i+= 1;
+				}
+			if(seta == 'd')
+				if(j < n-1){
+					map[i][j] = local;
+					j+= 1;
+				}
+			local = map[i][j];
+			map[i][j] = 'P';		
+		}
+	
+		private void tela(){
+			for(int b = 0; b < n; b++)
+				System.out.print(" _");
+			System.out.print("\n");
+			for(int a = 0; a < n; a++){
+				for(int b = 0; b < n; b++){
+					if(b == 0){
+						System.out.print('|');
+						if(a == n-1 && map[a][b] == ' ') System.out.print('_');
+						else System.out.print(map[a][b]);
+					}
+					else{
+						if(a == n-1 && map[a][b] == ' ') System.out.print(" _");
+						else System.out.print(" " + map[a][b]);
+					}
+				}
+				System.out.println("|");
 			}
-			System.out.println("|");
 		}
 		
+		public Jogo(long eventTime) {
+			super(eventTime);
+		}
+		
+		public void action() {
+			long tm = System.currentTimeMillis();
+			anda(comando[x++]);
+			if(local == 'g'){
+			   Random gerador = new Random(); 
+			   double numero = gerador.nextDouble();
+			   if(numero > 40){
+				   BattleControls bc = new BattleControls();
+				   bc.addEvent(new Round(tm + 1000));
+				   gramado(numero);
+			   }
+			}
+			tela();	
+			addEvent(new Jogo(tm + 2000));
+			}
+
+		public String description() {
+			return "";
+		}
 	}
-	
 	
 	public static void main(String[] args){
-		
-		
-		
 		JogoMapa m = new JogoMapa();
-		int x = 0;
-		while(m.comando[x] != '\0'){
-			m.anda(m.comando[x]);
-			if(m.local == 'g'){
-			   Random gerador = new Random(); 
-		       double numero = gerador.nextDouble();
-		       if(numero > 40) 
-		    	   m.gramado(numero);
-			}
-			m.tela();	
-			x++;
-		}
-		
+		long tm = System.currentTimeMillis();
+		m.addEvent(m.new Jogo(tm));
+		m.run();
 	}
-
 }
